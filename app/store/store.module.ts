fb343d9ec894a1a552f1e3d296d1d3958a52bd89
DESKTOP-431IURL\JUSTIN <chuksjustin2000@gmail.com>
2018-04-25 19:43:09 +0100

    Project setup, starting data model and store components and modules

---
+++ app/store/store.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { StoreComponent } from "./store.component";
+import { ModelModule } from "../model/model.module";
+
+@NgModule({
+    imports:[BrowserModule,FormsModule,ModelModule],
+    declarations: [StoreComponent],
+    exports:[StoreComponent]
+})
+
+export class StoreModule { }
