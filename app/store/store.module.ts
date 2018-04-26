5b7c687d661fc7f2c5e20d20ae54713e2fd8c685
DESKTOP-431IURL\JUSTIN <chuksjustin2000@gmail.com>
2018-04-26 19:16:04 +0100

    updating store features

--- app/store/store.module.ts
+++ app/store/store.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { StoreComponent } from "./store.component";
 import { ModelModule } from "../model/model.module";
+import { CounterDirective } from "./counter.directive";
 
 @NgModule({
     imports:[BrowserModule,FormsModule,ModelModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CounterDirective],
     exports:[StoreComponent]
 })
 
